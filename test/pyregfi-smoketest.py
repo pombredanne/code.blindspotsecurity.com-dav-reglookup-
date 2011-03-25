ff5447aea2f8eadf1a79d37c9642f2422e9ba5e5
tim <tim@cda35eab-ad64-493a-b152-d96cfa3efdff>
2011-03-25 15:48:27 +0000

    fixed some pyregfi parameter bugs fixed some iterator memory management problems updated smoketest script to use ctypes pyregfi

--- test/pyregfi-smoketest.py
+++ test/pyregfi-smoketest.py
@@ -1,39 +1,67 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
+import gc
 import pyregfi
 
-files = sys.argv[1:]
+def usage():
+    sys.stderr.write("USAGE: pyregfi-smoketest.py hive1 [hive2 ...]\n")
 
 
-def iter_values(key):
-    i = 0
-    for value in key.values():
-        i += 1
-        
-    return i
+# Uses the HiveIterator to walk all keys
+# Gathers various (meaningless) statistics to exercise simple attribute access
+# and to hopefully smoke out any bugs that can be identified by changing stats
+def iterTally(hive):
+    key_count = 0
+    key_lens = 0
+    key_rawlens = 0
+    value_count = 0
+    value_lens = 0
+    value_rawlens = 0
+
+    for k in hive:
+        key_count += 1
+        if k.name != None:
+            key_lens += len(k.name)
+        if k.name_raw != None:
+            key_rawlens += len(k.name_raw)
+
+        for v in k.values:
+            value_count += 1
+            if v.name != None:
+                value_lens += len(v.name)
+            if v.name_raw != None:
+                value_rawlens += len(v.name_raw)
+
+    print("  Counts: keys=%d, values=%d\n" % (key_count, value_count))
+    print("  Total name length: keys=%d, values=%d\n" % (key_lens, value_lens))
+    print("  Total raw name lengths: keys=%d, values=%d\n" % (key_rawlens, value_rawlens))
 
 
-for f in files:
-    rf = pyregfi.RegistryFile(f)
-    iter = rf.TreeIterator()
 
-    num_keys = 0
-    num_values = 0
-    # The iterator now walks the entire registry hive, depth-first
-    for key in iter:
-        #print key.key.keyname
-        num_keys +=1
-        num_values += iter_values(key)
+if len(sys.argv) < 2:
+    usage()
+    sys.exit(1)
 
-    print "keys: %d" % num_keys
-    print "values: %d" % num_values
+files = []
+for f in sys.argv[1:]:
+    files.append((f, open(f,"r+b")))
 
+tests = [("iterTally",iterTally),]
 
-iter = rf.TreeIterator()
-root = iter.current()
-for key in root.subkeys():
-    for subkey in key.subkeys():
-        for subsubkey in subkey.subkeys():
-            print key.key.keyname
+for hname,fh in files:
+    hive = pyregfi.Hive(fh)
+    for tname,t in tests:
+        tstr = "'%s' on '%s'" % (tname,hname)
+        print("##BEGIN %s:" % tstr)
+        t(hive)
+        print("##END %s; messages:" % tstr)
+        print(pyregfi.GetLogMessages())
+        print
+    hive = None
+    gc.collect()
 
+files = None
+tests = None
+gc.collect()
+print gc.garbage
