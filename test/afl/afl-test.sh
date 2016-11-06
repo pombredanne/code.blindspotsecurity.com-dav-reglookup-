fbc4a9fb5271d69645a47930c01d4a40a96a0055
tim <tim@cda35eab-ad64-493a-b152-d96cfa3efdff>
2016-11-06 22:36:20 +0000

    .

--- test/afl/afl-test.sh
+++ test/afl/afl-test.sh
@@ -2,7 +2,7 @@
 
 AFL_DIR=/usr/local/src/afl-2.35b
 
-TERMINAL='urxvt -geometry 80x25+0+0 -rv -e /bin/sh -c'
+TERMINAL='urxvt -geometry 80x25+0+0 -rv -hold -e /bin/sh -c'
 
 $TERMINAL "LD_LIBRARY_PATH=../../trunk/lib $AFL_DIR/afl-fuzz -i testcases -o findings -M master ../../trunk/src/reglookup -s @@"  &
 $TERMINAL "LD_LIBRARY_PATH=../../trunk/lib $AFL_DIR/afl-fuzz -i testcases -o findings -S slave1 ../../trunk/src/reglookup -s @@"  &
